62c5e896419fb471ebfc977a1ff7580634851073
post@kxe.sakura.ne.jp <post@kxe.sakura.ne.jp>
2021-08-21 20:46:58 +0900

    引数にて結合するディレクトリを指定するように修正

--- code/DRV-MR760/nmea_concat.py
+++ code/DRV-MR760/nmea_concat.py
@@ -1,19 +1,28 @@
+# -*- coding: utf-8 -*-
+
 import glob
+import sys
 
-files = glob.glob("./NMEA/*.NMEA")
+# 以下メイン
+def main():
+    files = glob.glob("./" + sys.argv[1] + "/*.NMEA")
 
-fout = open("all.NMEA", "w")
+    fout = open( sys.argv[1] + ".NMEA", "w")
 
-for file in files:
-    fin = open(file, 'r')
-    while True:
-        data = fin.readline()
-        # EofCheck
-        if not data:
-            break
-        fout.write(data)
-        fout.write("\n")
-        if data.startswith('<EOF/>') == True:
-            break;
-    fin.close
-fout.close
+    for file in files:
+        fin = open(file, 'r')
+        while True:
+            data = fin.readline()
+            # EofCheck
+            if not data:
+                break
+            fout.write(data)
+            fout.write("\n")
+            if data.startswith('<EOF/>') == True:
+                break;
+        fin.close
+    fout.close
+
+if __name__ == "__main__":
+    main()
+
